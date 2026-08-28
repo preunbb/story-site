78fc09f443f080bd268095d6f4b59ca21ac75bc8
preunbb <boozlejam@gmail.com>
2026-08-28 10:03:27 -0700

    wip

--- data/stories.js
+++ data/stories.js
@@ -1558,7 +1558,7 @@ window.DATA_STORIES = [
   },
   {
     id: 48,
-    wordCount: 13660,
+    wordCount: 13654,
     title: "Quality Control",
     brutalityRating: 5,
     cover: "assets/covers/quality_control_cover_v1.jpg",
